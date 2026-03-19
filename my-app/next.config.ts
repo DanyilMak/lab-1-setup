3c9f1a3ac324f39bedac774dbe23376762229448
DanyilMak <danyil.maksymchuk@oa.edu.ua>
2026-03-19 17:47:12 +0200

    Update next.config.ts

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
-  basePath: "/lab-1-setup",
-  assetPrefix: "/lab-1-setup/",
 };
 
 export default nextConfig;
