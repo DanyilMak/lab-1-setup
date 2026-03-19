a630678f7ac61e7877412b36456df8d50694c8ef
DanyilMak <danyil.maksymchuk@oa.edu.ua>
2026-03-19 17:51:20 +0200

    update

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
+
+  basePath: isGithubPages ? "/lab-1-setup" : "",
+  assetPrefix: isGithubPages ? "/lab-1-setup/" : "",
 };
 
 export default nextConfig;
