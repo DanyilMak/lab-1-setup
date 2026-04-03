316f77471fc08dfd8ecd0e3b6a90acbbba1d8283
DanyilMak <danyil.maksymchuk@oa.edu.ua>
2026-04-03 17:40:32 +0300

    add CI/CD

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const isGithubPages = process.env.GITHUB_PAGES === "true";
+const isDocker = process.env.DOCKER === "true";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  output: "export",
+
+  output: isDocker ? "standalone" : "export",
 
   basePath: isGithubPages ? "/lab-1-setup" : "",
   assetPrefix: isGithubPages ? "/lab-1-setup/" : "",
